b36f9ebc08d819d0afef1705008f1e3d2563b534
Angelina <pretzel318@gmail.com>
2023-02-14 14:53:53 +0400

    removed comments

--- src/core/ExelComponent.js
+++ src/core/ExelComponent.js
@@ -9,31 +9,25 @@ export class ExelComponent extends DomListener {
     this.prepare()
   }
 
-  // настройка компонента до init
   prepare() {}
 
-  // возврат шаблона компонента
   toHTML() {
     return ''
   }
 
-  // уведомление слушателей о событиях
   $emit(event, ...args) {
     this.emitter.emit(event, ...args)
   }
 
-  // подписка на события
   $on(event, fn) {
     const unsub = this.emitter.subscribe(event, fn)
     this.unsubscribers.push(unsub)
   }
 
-  // инициализация компонента и добавление слушателей
   init() {
     this.addDomListeners()
   }
 
-  // удаление компонента и слушателей
   destroy() {
     this.removeDomListeners()
     this.unsubscribers.forEach(unsub => unsub())
